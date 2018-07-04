470566596295d0ba46d4538f18a056ca245d8a17
richu shaji abraham <richursa@gmail.com>
2018-07-05 01:15:36 +0530

    added documentation

--- genBitonic.cpp
+++ genBitonic.cpp
@@ -1,8 +1,8 @@
-#include<iostream>
-#include<omp.h>
+#include<iostream>                                                                              //for std::cout ,std::cin
+#include<omp.h>                                                                                 
 using namespace std;
 
-void ascendingSwap(int index1 , int index2 , int *ar)
+void ascendingSwap(int index1 , int index2 , int *ar)                                           //swap two values such that they appear in ascending order in the array
 {
   if(ar[index2] < ar[index1])
   {
@@ -11,7 +11,7 @@ void ascendingSwap(int index1 , int index2 , int *ar)
       ar[index1] = temp;
   }
 }
-void decendingSwap(int index1 , int index2 , int *ar)
+void decendingSwap(int index1 , int index2 , int *ar)                                           //swap two values such that they appear in decending order in the array
 {
   if(ar[index1] < ar[index2])
   {
@@ -20,11 +20,11 @@ void decendingSwap(int index1 , int index2 , int *ar)
       ar[index1] = temp;
   }
 }
-    void bitonicSortFromBitonicSequence( int startIndex ,int lastIndex, int dir , int *ar )
+    void bitonicSortFromBitonicSequence( int startIndex ,int lastIndex, int dir , int *ar )     //form a increaseing or decreasing array when a bitonic input is given to the function
     {
         if(dir == 1)
         {
-            int counter = 0;
+            int counter = 0;                                                                    //counter to keep track of already swapped elements ,, parallelising this area results in poor performance due to overhead ,,need to fix
             int noOfElements = lastIndex - startIndex + 1;
             for(int j = noOfElements/2;j>0;j = j/2)
             {   counter =0;
@@ -45,7 +45,7 @@ void decendingSwap(int index1 , int index2 , int *ar)
                 }
             }
         }
-        else
+        else                                                                                    //decending sort
         {
             int counter = 0;
             int noOfElements = lastIndex - startIndex + 1;
@@ -70,36 +70,36 @@ void decendingSwap(int index1 , int index2 , int *ar)
         }
     
     }
-void bitonicSequenceGenerator(int startIndex , int lastIndex , int *ar)
+void bitonicSequenceGenerator(int startIndex , int lastIndex , int *ar)                         //generate a bitonic sequence  from a a random order
 {
     int noOfElements = lastIndex - startIndex +1;
-    int counter =0;
       for(int j = 2;j<=noOfElements;j = j*2)
             {   
-                counter =0;
-                #pragma omp parallel for
+                #pragma omp parallel for                                                         //parallel implementation results in most performance gains here
                 for(int i=0;i<noOfElements;i=i+j)
                 {
-                 if(((i/j)%2) ==0)
+                 if(((i/j)%2) ==0)                                                               //extra computation results in drastically lower performance ,need to fix
                  {
                      bitonicSortFromBitonicSequence(i,i+j-1,1,ar);
-                     counter++;
                  }   
                  else
                  {
                      bitonicSortFromBitonicSequence(i,i+j-1,0,ar);
-                     counter++;
                  }
                 }
             }
 }
-int main()
-{
-    omp_set_dynamic(0);
-    omp_set_num_threads(3);
+int main()                                                                                        //main driver function
+{   
+    omp_set_dynamic(0);                                                                          //disabled so that the os doesnt override the thread settings
+    cout<<"enter the number of threads ";                                                           
+    int threads;
+    cin>>threads;
+    omp_set_num_threads(threads);                                                                //set the no of threads
     int n;
+    cout<<"enter the number of elements to be sorted (number should be in the order of 2^n";     //try to fix this issue 
     cin>>n;
-    int *ar = new int[n];
+    int *ar = new int[n];                                                                               
     for(int i=0;i<n;i++)
     {
         cin>>ar[i];
